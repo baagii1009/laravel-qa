30f26dc4bcdabd30c5031eaf0b32d69c4c7e9a2e
Batmunkh ts <batmunkh@erdenetmc.mn>
2018-12-05 15:35:58 +0800

    Implement delete

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -79,7 +79,7 @@ class QuestionsController extends Controller
     {
         $question->update($request->only('title', 'body'));
 
-        return redirect('/questions')->with('success', "Your question has bee updated.");
+        return redirect('/questions')->with('success', "Your question has been updated.");
     }
 
     /**
@@ -90,6 +90,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect('/questions')->with('success', "Your question has been deleted.");
     }
 }
