b1ad2e0ae128e1f81030cb78e2188bf722b43e8e
Batmunkh ts <batmunkh@erdenetmc.mn>
2018-12-05 13:26:28 +0800

    Add author info & question creation date

--- app/Question.php
+++ app/Question.php
@@ -15,5 +15,15 @@ class Question extends Model
     public function SetTitleAttribute($value) {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
     }    
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
