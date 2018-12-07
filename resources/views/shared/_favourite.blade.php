0267475370dd016ecf8e4b5ca6707b609a2e4e00
Batmunkh ts <batmunkh@erdenetmc.mn>
2018-12-07 12:14:11 +0800

    Implement DRY principles on the app views

---
+++ resources/views/shared/_favourite.blade.php
@@ -0,0 +1,21 @@
+<a 
+    title="Click to mark as favourite model (Click again to undo)" 
+    class="favourite mt-2 {{ Auth::guest() ? 'off' : ($model->is_favourited ? 'favourited' : '') }}"
+    onclick="event.preventDefault();document.getElementById('favourite-question-{{ $model->id }}').submit();"
+>
+    <i class="fas fa-star fa-2x"></i>
+    <span class="favourites-count">
+        {{ $model->favourites_count }}
+    </span>
+</a>
+<form 
+    id="favourite-question-{{ $model->id }}" 
+    action="/questions/{{ $model->id }}/favourites" 
+    method="POST" 
+    style="display:none;"
+>
+    @csrf 
+    @if ($model->is_favourited)
+        @method('DELETE')
+    @endif
+</form>
