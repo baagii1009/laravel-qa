bb15c752a67a84ff477287acc5d5bdff5c1f11ab
Batmunkh ts <batmunkh@erdenetmc.mn>
2018-12-07 16:16:51 +0800

    created vue answer comp

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -45,6 +45,13 @@ use Illuminate\Http\Request;
             'body' => 'required',
         ]));
 
+        if ($request->expectsJson()){
+            return response()->json([
+                'message' => 'Your answer has been updated',
+                'body_html' => $answer->body_html
+            ]);
+        }
+
         return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
     }
      /**
